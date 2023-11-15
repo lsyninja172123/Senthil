76945329e2555f3a9448e57d5859a5c096a07bcd
lsyninja <lsyninja172123@gmail.com>
2023-11-15 08:33:04 +0530

    "sub developed"

--- Test/src/main/java/calCreation/UiDevelopment.java
+++ Test/src/main/java/calCreation/UiDevelopment.java
@@ -4,6 +4,9 @@ public class UiDevelopment {
 	public static void add() {
    System.out.println("UI developed");
 	}
+	public static void sub() {
+		   System.out.println("sub fields debeloped ");
+			}
 
 	public static void main(String[] args) {
     add();
