b252f72c01f8ba529cc458784ffc640bd5c93982
lsyninja <lsyninja172123@gmail.com>
2023-11-15 08:30:43 +0530

    "Initial commit"

---
+++ Test/src/main/java/calCreation/UiDevelopment.java
@@ -0,0 +1,12 @@
+package calCreation;
+
+public class UiDevelopment {
+	public static void add() {
+   System.out.println("UI developed");
+	}
+
+	public static void main(String[] args) {
+    add();
+	}
+
+}
