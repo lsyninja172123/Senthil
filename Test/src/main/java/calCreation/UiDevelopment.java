78a0f65f33ff89946b8410a9f6d4c9966d1c140d
lsyninja <lsyninja172123@gmail.com>
2023-11-30 21:28:49 +0530

    sysout added

--- Test/src/main/java/calCreation/UiDevelopment.java
+++ Test/src/main/java/calCreation/UiDevelopment.java
@@ -1,8 +1,15 @@
 package calCreation;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class UiDevelopment {
 	public static void add() {
-   System.out.println("UI developed");
+   System.out.println("UI1 developed");
+   System.out.println("UI2 developed");
+
 	}
 	public static void sub() {
 		   System.out.println("sub fields developed ");
